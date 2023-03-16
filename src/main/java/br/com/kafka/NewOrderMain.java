af7f02c105692000050397d0485a1b96b96a00a7
rookie-leo <proleonardopa@gmail.com>
2023-03-16 09:47:48 -0300

    Vários consumidores e produtores

--- src/main/java/br/com/kafka/NewOrderMain.java
+++ src/main/java/br/com/kafka/NewOrderMain.java
@@ -1,11 +1,11 @@
 package br.com.kafka;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
@@ -15,15 +15,20 @@ public class NewOrderMain {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "13,22,789.50";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+        var email = "Obrigado por comprar conosco! Estamos processando seu pedido!";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
 
-        producer.send(record, (data, ex) -> {
+        Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
 
             System.out.println("SUCESSO " + data.topic() + "::partition " + data.partition() + "/ offset " + data.offset() + " timestamp " + data.timestamp());
-        }).get();
+        };
+
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties() {
