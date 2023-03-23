b1d5492da5bb86c8978ff0265a95905339e9cec0
rookie-leo <proleonardopa@gmail.com>
2023-03-23 11:07:32 -0300

    Migrando o log

--- src/main/java/br/com/kafka/services/KafkaService.java
+++ src/main/java/br/com/kafka/services/KafkaService.java
@@ -11,16 +11,25 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 public class KafkaService implements Closeable {
     private final KafkaConsumer<String, String> consumer;
     private final ConsumerFunction parse;
 
     KafkaService(String groupId, String topic, ConsumerFunction parse) {
+        this(parse, groupId);
+        consumer.subscribe(Collections.singletonList(topic));
+    }
+
+    KafkaService(String groupId, Pattern topic, ConsumerFunction parse) {
+        this(parse, groupId);
+        consumer.subscribe(topic);
+    }
+
+    private KafkaService(ConsumerFunction parse, String groupId) {
         this.parse = parse;
         this.consumer = new KafkaConsumer<>(properties(groupId));
-        consumer.subscribe(Collections.singletonList(topic));
-
     }
 
     void run() {
